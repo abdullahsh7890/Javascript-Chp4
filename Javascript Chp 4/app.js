7e47e05bcf48cd4de4f4d45a3d282616ddb61707
Abdullah Bin Shahbaz <154223491+abdullahsh7890@users.noreply.github.com>
2024-03-01 22:53:37 +0500

    Add files via upload

---
+++ Javascript Chp 4/app.js
@@ -0,0 +1,19 @@
+// Q1 
+var one,two,three
+
+// Q2
+
+// legal variable names
+
+var $abdullah,_abdullahbin,abdullahBinShahbaz,abdullah_bin_shahbaz,AbdullahBinShahbaz
+
+// var 1abdullahBinShahbaz,-AbdullahBinShahbaz,8abdullahBinShahbaz3,67378888,*abdullahBinShahbaz  // illegel variable names
+  
+// Q3
+
+document.write("<br/>Rules for naming JS variables")
+
+document.write("<br/><br/>Variable names can only contain , numbers, $ and _ , For example : $my_1stVariable")
+document.write("<br/>Variable must begin with a letter, $ or _ . For example : $name, _name or name")
+document.write("<br/>Variable names are case sensitive")
+document.write("<br/>Variable names should not be JS keywords")
